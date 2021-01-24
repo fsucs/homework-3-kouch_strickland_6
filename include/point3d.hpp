ca9b73c03eaedc4e3375610d8bb1a7b2ba7f81bc
Travis Strickland <77255475+Travisdstrickland@users.noreply.github.com>
2021-01-24 14:21:15 -0500

    Update point3d.hpp

--- include/point3d.hpp
+++ include/point3d.hpp
@@ -5,5 +5,25 @@
 #ifndef POINT3D_H
 #define POINT3D_H
 
+class Point3D{
 
+public:
+
+    Point3D();
+    Point3D(double x, double y, double z);
+
+    void SetCoordinates(double x, double y, double z);
+
+    double Distance(const Point3D& point2) const;
+
+    void GetXCoord();
+    void GetYCoord();
+    void GetZCoord();
+
+private:
+
+    double XCoord;
+    double YCoord;
+    double ZCoord;
+};
 #endif

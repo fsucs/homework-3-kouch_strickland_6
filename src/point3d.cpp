63aafca189ead882da800f3c60ceb2e292f71bdb
Travis Strickland <77255475+Travisdstrickland@users.noreply.github.com>
2021-01-24 14:20:31 -0500

    Update point3d.cpp

--- src/point3d.cpp
+++ src/point3d.cpp
@@ -3,3 +3,27 @@
  */
 
 #include "../include/point3d.hpp"
+#include <iomanip>
+#include <iostream>
+
+Point3D::Point3D(double x, double y, double z)
+{
+    XCoord = x;
+    YCoord = y;
+    ZCoord = z;
+}
+
+void Point3D::GetXCoord()
+{
+    return XCoord;
+}
+
+void Point3D::GetYCoord()
+{
+    return YCoord;
+}
+
+void Point3D::GetZCoord()
+{
+    return ZCoord;
+}

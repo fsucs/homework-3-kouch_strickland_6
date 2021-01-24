d525e4b4645a4bb726dedfb4431efefe525de8a9
Travis Strickland <77255475+Travisdstrickland@users.noreply.github.com>
2021-01-24 14:19:38 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 /** 
     @mainpage Outer Space Communications
-
     @author Author
-
     What does this program do in short? Perhaps the name of the project goes here?
 */
 
@@ -10,13 +8,26 @@
     \brief The code to solve the problem should be inside main.cpp
     
     Long explination goes here...
-
     Requires: C++11
 */
 
-# include "../include/point3d.hpp"
-
+# include "Point3D.hpp"
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
+
+    Point3D a1, a2;
+    ifstream infile;
+
+    infile.open("test.sh");
+
+    if(infile.is_open())
+    {
+        getline(infile, a1.Set)
+    }
+
+    infile.close();
     return 0;
 }
